de98e2fd2384396cd8a14640fbf92855db11c23c
Sami Keijonen <sami.keijonen@foxnet.fi>
2019-11-24 23:50:44 +0200

    Build assets.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '8bb68277c5687951148226b64aeebaa7');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '39dbccd17961107d7ab9fcac32b2dc1e');
